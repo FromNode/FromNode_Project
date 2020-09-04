2f905c92c960bfe5ffbec6c263ed768d25655c2e
SeaJungg <wantedsaea@likelion.org>
2020-09-04 19:43:57 +0900

    모델작업중

--- NodeApp/models.py
+++ NodeApp/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 from ProjectApp import models
 from UserApp import models
+from django.contrib.auth.models import User
 
 class Nodes(models.Model):
     ID = models.CharField(primary_key = True, default=0, unique=True) #노드별 고유 코드
     createdDate = models.DateTimeField(auto_now=True)
     fileObj = models.FileField(upload_to="media", blank=False) #이 노드에 저장되어 있는 파일
     previousID = models.ForeignKey('self',on_delete=SET_NULL, null=True, blank=True) #이전 노드
-    ownerProjectID = models.ForeignKey('ProjectApp.projects'on_delete=models.CASCADE)#이 노드가 속해 있는 프로젝트
-    whoIsOwner = models.ForeignKey('',)#이 노드를 만든 회원
+    ownerProjectID = models.ForeignKey('ProjectApp.projects', on_delete=models.CASCADE)#이 노드가 속해 있는 프로젝트
+    whoIsOwner = models.ForeignKey(User,on_delete = SET_NULL, null=True, blank=True)#이 노드를 만든 회원
+    
     ''' 
     def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
         return ''.join(random.choice(chars) for _ in range(size))
--- ProjectApp/models.py
+++ ProjectApp/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from NodeApp import models
+from django.contrib.auth.models import User
 
 class Projects(models.Model):
     ID = models.CharField(default=0, unique=True)
     name = models.CharField(max_length=255)
-    whoIsOwner = models.ForeignKey(NodeApp.,on_delete=SET_NULL, null=True, blank=True)
+    whoIsOwner = models.ForeignKey(User, on_delete=SET_NULL, null=True, blank=True)
