1546a230f9a028b11d78eb03aeec81ea4c3e0608
seulong <kjkj3468@naver.com>
2020-09-04 16:04:33 +0900

    setting scss

--- FromNode/urls.py
+++ FromNode/urls.py
@@ -1,25 +1,10 @@
-"""FromNode URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main',include('mainapp.urls')),
-    path('node',include('nodeapp.urls')),
-    path('project',include('projectapp.urls')),
-    path('user',include('userapp.urls')),
+    path('main',include('MainApp.urls')),
+    path('node',include('NodeApp.urls')),
+    path('project',include('ProjectApp.urls')),
+    path('user',include('UserApp.urls')),
 ]
