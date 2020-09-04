f903fb77825cc8a2b02f952ff8b216a53bcb6f4d
SeaJungg <wantedsaea@gmail.com>
2020-09-04 18:51:03 +0900

    [Back-end] 모델 작업중

--- ProjectApp/models.py
+++ ProjectApp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from NodeApp import models
 
-# Create your models here.
+class Projects(models.Model):
+    ID = models.CharField(default=0, unique=True)
+    name = models.CharField(max_length=255)
+    whoIsOwner = models.ForeignKey(NodeApp.,on_delete=SET_NULL, null=True, blank=True)
