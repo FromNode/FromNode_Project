93ec2910d097ff6e53e6d82d94a529fc169e862f
seulong <kjkj3468@naver.com>
2020-09-05 17:25:00 +0900

    Edit git ignore feat pizza

--- ProjectApp/urls.py
+++ ProjectApp/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+from . import views 
 urlpatterns = [
     path('project_list/', views.show_project_list, name="show_project"),
     path('create_project/', views.create_project, name="create_project")
