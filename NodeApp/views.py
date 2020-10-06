bf8fd1440f7e7a9c3b2f31171d71d97aba41ce87
SeaJungg <wantedsaea@likelion.org>
2020-10-07 03:17:05 +0900

    흐름도대로 만드는중인데 파일상세내용 추가된거 깜박하고 만들었음;;

--- NodeApp/views.py
+++ NodeApp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Nodes
 
 def show_project_detail(request):
-    return render(request, 'NodeApp/project_detail.html')
+    node_all = Nodes.objects.all
+    return render(request, 'NodeApp/project_detail.html', {'node_all':node_all})
 
 def show_upload(request):
     return render(request, 'NodeApp/upload.html')
--- ProjectApp/admin.py
+++ ProjectApp/admin.py
@@ -1,8 +1,4 @@
 from django.contrib import admin
 from .models import Projects
 
-<<<<<<< HEAD
 admin.site.register(Projects)
-=======
-admin.site.register(Projects)
->>>>>>> 93ec2910d097ff6e53e6d82d94a529fc169e862f
