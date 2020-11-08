c67045ae673399855e75713f75d6522ab090f193
SeaJungg <wantedsaea@likelion.org>
2020-11-09 01:01:00 +0900

    secret키 분리

--- FromNode/settings.py
+++ FromNode/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
-import os
+import os, json
+from django.core.exceptions import ImproperlyConfigured
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -8,8 +10,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u1mcw%y2xk@sb088%*tr0u57cim_(4g!rbn_zdoy*9vgfuq0q9'
+secret_file = os.path.join(BASE_DIR, 'secrets.json')
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
 
+def get_secret(setting, secrets = secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable 카톡에 씨크릿키 파일 있음. 깃이그노어 있는 폴더에 붙여넣엉".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
