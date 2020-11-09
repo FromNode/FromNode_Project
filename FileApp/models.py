fb263c009adeb3ecaa1fde50fddc63c5b575a908
New-TW <wlsxodnjs1@gmail.com>
2020-11-09 10:26:56 +0900

    file_upload

--- FileApp/models.py
+++ FileApp/models.py
@@ -1,9 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
+from random import choice
+import string
+
+
+def random_code():
+    _LENGTH = 8
+    strings = ['1','2','3','4','5','6','7','8','9']
+    string_pool = string.digits
+    result = ''
+    for i in range(_LENGTH):
+        if i ==0:
+            result+=choice(strings)
+        result += choice(string_pool)
+    return result
 
 class Files(models.Model):
     fileName = models.CharField(max_length=1023)
     createdDate = models.DateTimeField(auto_now=True)
-    Code = models.CharField(primary_key = True, max_length=255, default=0, unique=True)
+    Code = models.CharField(primary_key = True, max_length=255, default=random_code, unique=True)
     whoIsOwner = models.ForeignKey(User,on_delete = models.SET_NULL, null=True, blank=True)
     ownerPCode = models.ForeignKey('ProjectApp.projects', on_delete=models.CASCADE)
--- FileApp/views.py
+++ FileApp/views.py
@@ -32,7 +32,7 @@ def show_file_list(request,project_id):
     file_obj = Files.objects.filter(ownerPCode=project_id)
     if len(file_obj) == 0:
         empty = '추적한 파일이 없습니다'
-    return render(request, 'FileApp/file_list.html', {'file_obj':file_obj,'proj_user':proj_user,'empty':empty})
+    return render(request, 'FileApp/file_list.html', {'project':project.id, 'file_obj':file_obj,'proj_user':proj_user,'empty':empty})
 
 # def form_create_new_file(request):
 #     return render(request, 'FileApp/form_create_new_file.html')
@@ -40,12 +40,11 @@ def show_file_list(request,project_id):
 # def show_upload(request):
 #     return render(request, 'FileApp/upload.html')
 
-# def create_new_file(request):
-#     file_obj = Files()
-#     file_obj.fileName = "따뜻한라떼"
-#     file_obj.Code = random.randint(0,0xffffff)
-#     file_obj.whoIsOwner = User.objects.get(username = 'sea')
-#     file_obj.ownerPCode = Projects.objects.get(name = 'proscons')
-#     file_obj.save()
-#     return redirect('project_detail')
+def create_new_file(request):
+    file_obj = Files()
+    file_obj.fileName = request.POST['fileName']
+    file_obj.whoIsOwner = request.user
+    file_obj.ownerPCode = Projects.objects.get(id=request.POST['pk'])
+    file_obj.save()
+    return render(request,'ProjectApp/project_list.html')
     
