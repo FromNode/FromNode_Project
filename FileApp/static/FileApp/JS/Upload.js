304b72c53a164aac2a041217a3ecd91049772a3a
seulong <choiseulong@gmail.com>
2020-11-09 10:41:19 +0900

    [Front-end] file_list 수정 업로드 기능 가지러 갑니다

--- FileApp/static/FileApp/JS/Upload.js
+++ FileApp/static/FileApp/JS/Upload.js
@@ -66,16 +66,3 @@ function updateThumnail(dropZoneElement, file){
         thumnailElement.style.backgroundImage = null;
     }
 }
-
-
-const openButton = document.querySelector(".openModal");
-const openButton_Bottom = document.querySelector(".section4Btn");
-const modal = document.querySelector(".loginModal");
-const overlay = modal.querySelector(".modal_Overlay");
-const toggleModal = () => {
-    modal.classList.toggle("hidden");
-}
-overlay.addEventListener("click", toggleModal);
-openButton.addEventListener("click", toggleModal);
-openButton_Bottom.addEventListener("click", toggleModal);
-alert("te3");
