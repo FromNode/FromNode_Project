4c3090b7955d99f8809d9fa6bffcda7b0efabb2a
New-TW <wlsxodnjs1@gmail.com>
2020-11-14 05:38:02 +0900

    [Back-end]file_list css 멕임+자잘한 에러 수정

--- FileApp/views.py
+++ FileApp/views.py
@@ -18,18 +18,33 @@ import random
 #     return render(request, 'ProjectApp/project_list.html', {'proj_obj' : proj_obj})
 
 def show_file_list(request,project_id):
+    proj_obj = []
+    User_Profile = []
+    User_Projects = []
+    
+    if request.user.is_authenticated:
+        User_Profile = Profile.objects.get(user=request.user)
+        # filter는 쿼리셋 메소드를 가져오니까 get으로 값을 불러오세요!!!!!!
+        User_Projects = User_Profile.projects.split(',')
+    else:
+        pass
+
+
+    for i in User_Projects:
+        proj_obj += Projects.objects.filter(name=i)
+
     project = Projects.objects.get(id = project_id)
     pro_name = project.name
     # detail pro name 뽑아오는 과정
-    proj_obj = []
+    proj_objs = []
     proj_user = []
     first_file = []
     empty = ''
     # for문 위한 초기 선언
     for x in Profile.objects.all():
         a = x.projects.split(',')
-        proj_obj = a
-        if pro_name in proj_obj:
+        proj_objs = a
+        if pro_name in proj_objs:
             proj_user.append(x.user)
     # for문 지나서 project 가진 user 정보들이 list로 뽑힙니다
     file_obj = Files.objects.filter(ownerPCode=project_id)
@@ -37,7 +52,7 @@ def show_file_list(request,project_id):
     
     if len(file_obj) == 0:
         empty = '추적한 파일이 없습니다'
-    return render(request, 'FileApp/file_list.html', {'pro_name':pro_name,'project':project.id, 'file_obj':file_obj,'proj_user':proj_user,'empty':empty})
+    return render(request, 'FileApp/file_list.html', {'proj_obj':proj_obj,'pro_name':pro_name,'project':project.id, 'file_obj':file_obj,'proj_user':proj_user,'empty':empty})
 
 # def form_create_new_file(request):
 #     return render(request, 'FileApp/form_create_new_file.html')
@@ -46,16 +61,17 @@ def show_file_list(request,project_id):
 #     return render(request, 'FileApp/upload.html')
 
 def create_new_file(request):
-    filename, fileExtension = request.FILES['myFile']
+    filename = request.FILES['myFile']
+    fileExtension = str(filename).split('.')[1]
     file_obj = Files()
-    file_obj.fileName = request.POST['fileName']
+    file_obj.fileName = str(filename).split('.')[0]
     file_obj.whoIsOwner = request.user
     file_obj.ownerPCode = Projects.objects.get(id=request.POST['pk'])
-    if(fileExtension == '.pptx'):
+    if(fileExtension == 'pptx'):
         file_obj.image = 'ppt'
-    elif(fileExtension =='.doc' or fileExtension == '.docx'):
+    elif(fileExtension =='doc' or fileExtension == 'docx'):
         file_obj.image = 'word'
-    elif(fileExtension=='.pdf'):
+    elif(fileExtension=='pdf'):
         file_obj.image = 'pdf'
     else:
         file_obj.image = 'etc'
