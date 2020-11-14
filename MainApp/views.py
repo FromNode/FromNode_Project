b47ad5e83571bc9bf6afe080fc8521b2346cc290
SeaJungg <wantedsaea@likelion.org>
2020-11-14 13:22:47 +0900

    로그아웃이 돼요

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib import auth
 from UserApp.forms import LoginForm
 # 자동로그인
 def index(request):
@@ -7,4 +8,9 @@ def index(request):
         # return redirect('project:project_list')
     return render(request, 'MainApp/index.html')
 
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('index')
+    return render(request, 'MainApp/index.html')
     
