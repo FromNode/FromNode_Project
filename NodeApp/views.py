963dc06680bbfec538b1548c02eebdece8bd4e4a
New-TW <wlsxodnjs1@gmail.com>
2021-02-02 14:00:52 +0900

    download_complete

--- NodeApp/views.py
+++ NodeApp/views.py
@@ -190,12 +190,7 @@ def download_view(request, pk):
     file_url = urllib.parse.unquote(url)
     if os.path.exists(file_url):
         with open(file_url, 'rb') as fh:
-            print(node)
-            print(node.fileObj)
-            print(node.filename)
-            print(node.pk)
             quote_file_url = urllib.parse.quote(node.filename.encode('utf-8'))
-            print(quote_file_url)
             response = HttpResponse(fh.read(), content_type=mimetypes.guess_type(file_url)[0])
             response['Content-Disposition'] = 'attachment;filename*=UTF-8\'\'%s' % quote_file_url
             return response
