c3e3dae3b78c281d0b2724ffae26de47fb52eaea
jaehoonkim.dev@gmail.com <jaehoonkim.dev@gmail.com>
2021-02-15 21:42:14 +0900

    feat: convert module(convert, show_paragraph)

--- NodeApp/textualization/convert.py
+++ NodeApp/textualization/convert.py
@@ -1,1 +1,17 @@
-def convert
+from docx import Document
+import docx
+
+
+def convert(target_document):
+    # FileName Extension
+    fn_extension = target_document.split(".")[-1]
+    if fn_extension == "docx":
+        return docx.Document(target_document)
+
+
+def show_paragraph(converted_document, index):
+    docu_type_str = str(type(converted_document))
+    if "docx.document.Document" in docu_type_str:
+        print("h")
+    else:
+        raise Exception("Unsupported file extension. Only use docx or ...")
