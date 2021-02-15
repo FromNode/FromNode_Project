c545dfb6e1ec1aaa2d5b09680bb23a2781f340ce
jaehoonkim.dev@gmail.com <jaehoonkim.dev@gmail.com>
2021-02-15 21:50:15 +0900

    fix: show_paragraph function from convert

--- NodeApp/textualization/convert.py
+++ NodeApp/textualization/convert.py
@@ -12,6 +12,11 @@ def convert(target_document):
 def show_paragraph(converted_document, index):
     docu_type_str = str(type(converted_document))
     if "docx.document.Document" in docu_type_str:
-        print("h")
+        if index == "all":
+            for x, paragraph in enumerate(converted_document.paragraphs):
+                print(str(x) + " : " + paragraph.text)
+            return None
+        elif type(index) == int:
+            return converted_document.paragraphs[index].text
     else:
         raise Exception("Unsupported file extension. Only use docx or ...")
