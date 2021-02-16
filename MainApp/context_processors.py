8b07c875fd91c48ed105b6232d96b628be55dc7a
Hyeseon Lee <hyesun1999@gmail.com>
2021-02-16 22:12:10 +0900

    [frontend] feat : Edit base navbar design

--- MainApp/context_processors.py
+++ MainApp/context_processors.py
@@ -6,6 +6,11 @@ def project_list(request):
         unliked_proj = User.Joined_Unliked_Projects.all()
         liked_proj = User.Joined_Liked_Projects.all()
         all_proj = unliked_proj | liked_proj
-        return {'proj_obj_all':all_proj}
-    else:
-        return {'proj_obj_all':'none'}
+        profile_img_url = User.Profile.profile_image.url
+        username = User.Profile.nickname
+        return {'proj_obj_all':all_proj,
+                'profile_img_url':profile_img_url,
+                'username':username}
+    # else:
+    #     return {'proj_obj_all':'none',
+    #             'profile_img_url':''}
