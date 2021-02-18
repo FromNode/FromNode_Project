78e46500a42fd6a83f4c015679a82f89093347cb
Jaehoon Kim <43606451+jaehoonkimm@users.noreply.github.com>
2021-02-18 18:45:25 +0900

    Revert "Issue/166 add summary function"

--- NodeApp/summarization/summary.py
+++
@@ -1,9 +0,0 @@
-# Pororo 미설치 가상환경인 경우, pip install pororo
-from pororo import Pororo
-
-
-def summary(target_text, language="ko"):
-    if type(target_text) != str:
-        raise Exception("can only summary str type as arguments")
-    summ = Pororo(task="summarization", model="abstractive", lang=language)
-    return summ(target_text)
