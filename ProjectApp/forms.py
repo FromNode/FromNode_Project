f2f270b5524c995efdef29404de6c69923df4ab7
SeaJungg <wantedsaea@likelion.org>
2021-06-05 23:22:09 +0900

    forms.py시도했던거 삭제, 비번규칙 약하게

--- ProjectApp/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from UserApp.models import User
-
-
-class UserListForm(forms.ModelForm):
-    peer = forms.ModelMultipleChoiceField(queryset=User.objects.all(), required=False)
