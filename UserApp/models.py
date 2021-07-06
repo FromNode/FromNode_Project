887250ffa9ee192a05510f40b395aeb0d47d8b52
TaewonJin <wlsxodnjs1@gmail.com>
2021-07-06 13:29:08 +0900

    models_fix

--- UserApp/models.py
+++ UserApp/models.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from ProjectApp.models import Projects
 
 media_path = settings.MEDIA_ROOT
 
